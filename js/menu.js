edc784e45ab74c4947baac2412fe46d134424aa0
SERHII YEROKHIN <sergeye05@ukr.net>
2022-12-02 11:21:55 +0200

    сом

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,20 @@
     openmenuBtn: document.querySelector("[data-menu-open]"),
     closemenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
+    body: document.querySelector("body"),
+    menuList: document.querySelector(".mob-menu-item"),
   };
 
   refs.openmenuBtn.addEventListener("click", togglemenu);
   refs.closemenuBtn.addEventListener("click", togglemenu);
+  refs.closemenuBtn.addEventListener("click", togglemenu);
 
   function togglemenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.menu.classList.toggle("no-scroll");
+  }
+   function removemenu() {
+    refs.menu.classList.add("is-hidden");
+    refs.menu.classList.remove("no-scroll");
   }
 })();
